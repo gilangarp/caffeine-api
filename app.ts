b9778d6337654b2cba7dd799fcba99f7873004b0
gilangarp <gilangarya2001.ap@gmail.com>
2025-01-19 17:32:23 +0700

    fix swagger options

--- app.ts
+++ app.ts
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://caffeine-app-iota.vercel.app"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
--- src/configs/swaggerOptions.ts
+++ src/configs/swaggerOptions.ts
@@ -1,21 +1,21 @@
-import { Options } from 'swagger-jsdoc';
+import { Options } from "swagger-jsdoc";
 
 const swaggerOptions: Options = {
   swaggerDefinition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'CAFFEINE-API',
-      version: '1.0.0',
-      description: 'Dokumentasi API menggunakan Swagger dan Express',
+      title: "CAFFEINE-API",
+      version: "1.0.0",
+      description: "Dokumentasi API menggunakan Swagger dan Express",
     },
     servers: [
       {
-        url: 'http://localhost:8080',  
-        description: 'Development server'
-      }
+        url: ["http://localhost:8080", "https://caffeine-api-pi.vercel.app"],
+        description: "Development server",
+      },
     ],
   },
-  apis: ['./src/router/**/*.ts'],
+  apis: ["./src/router/**/*.ts"],
 };
 
 export default swaggerOptions;
